c0866fb1b6243207ae9dc9f571c09d1be3d168e5
Phumai <mcc3939@gmail.com>
2021-03-31 11:48:06 +0700

    mock data

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -25,14 +25,16 @@ class ProductModel {
 List<ProductModel> productList = [
   ProductModel(
     id: "1",
-    title: "Apartment Room, Interior Design",
-    detail: "Bedroom Bed Apartment Room Interior Design",
+    title: "Sofa",
+    detail:
+        "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     prize: 19720,
     amount: 50,
-    cover: "https://picsum.photos/500",
+    cover:
+        "https://i.pinimg.com/564x/48/d9/cf/48d9cf66f2178ea9dc86c16ac7e75baf.jpg",
     images: [
-      "https://picsum.photos/768/1024",
-      "https://picsum.photos/1024/768",
+      "https://i.pinimg.com/564x/1b/b6/78/1bb678b6ad9e574aa3306cfbe73df3b7.jpg",
+      "https://i.pinimg.com/564x/7c/3a/66/7c3a66f3b61faa4cbd41b29db01d470b.jpg",
     ],
     colors: [
       Color(0xFFD2B99B),
@@ -42,14 +44,16 @@ List<ProductModel> productList = [
   ),
   ProductModel(
     id: "2",
-    title: "Business Desk",
-    detail: "Office Sitting Room Executive Business Desk",
+    title: "Bed",
+    detail:
+        "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     prize: 29990,
     amount: 50,
-    cover: "https://picsum.photos/768",
+    cover:
+        "https://i.pinimg.com/564x/7a/09/ca/7a09cae99846a0348f69c2f1f2a539ce.jpg",
     images: [
-      "https://picsum.photos/768",
-      "https://picsum.photos/768",
+      "https://i.pinimg.com/564x/fa/a3/0b/faa30b74db4674ec9371a54fd5c33134.jpg",
+      "https://i.pinimg.com/736x/9b/07/00/9b0700ee0238aac13374227896605f4d.jpg",
     ],
     colors: [
       Color(0xFF664429),
@@ -58,14 +62,16 @@ List<ProductModel> productList = [
   ),
   ProductModel(
     id: "3",
-    title: "Kitchen Home",
-    detail: "Kitchen Home Interior Modern Room Floor Furniture",
+    title: "Bathroom",
+    detail:
+        "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     prize: 34200,
     amount: 50,
-    cover: "https://picsum.photos/768/1024",
+    cover:
+        "https://i.pinimg.com/564x/68/ab/2f/68ab2fb48cabc6f5095294474ac03ff3.jpg",
     images: [
-      "https://picsum.photos/1024/768",
-      "https://picsum.photos/1024/768",
+      "https://i.pinimg.com/564x/fc/90/62/fc90622d26e4b8101f4c6b98abfce23e.jpg",
+      "https://i.pinimg.com/564x/f6/9c/8d/f69c8dc4df6c8e6c8e81ebbdd20763fa.jpg",
     ],
     colors: [
       Color(0xFF664429),
@@ -74,13 +80,15 @@ List<ProductModel> productList = [
   ProductModel(
     id: "4",
     title: "Brick Wall Room",
-    detail: "Brick Wall Room Interior Design Furniture",
+    detail:
+        "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     prize: 19720,
     amount: 50,
-    cover: "https://picsum.photos/1024",
+    cover:
+        "https://i.pinimg.com/564x/13/26/48/132648cdf788c38a3f454e56ab3185b8.jpg",
     images: [
-      "https://picsum.photos/1024",
-      "https://picsum.photos/1024",
+      "https://i.pinimg.com/564x/32/2d/e6/322de67dd7ca99f4606e9ba241c2e198.jpg",
+      "https://i.pinimg.com/564x/98/fe/ab/98feabf17e0e0b733b7f4aa3a65cec0d.jpg",
     ],
     colors: [
       Color(0xFFD2B99B),
@@ -89,14 +97,16 @@ List<ProductModel> productList = [
   ),
   ProductModel(
     id: "5",
-    title: "Apartment Room, Interior Design",
-    detail: "Bedroom Bed Apartment Room Interior Design",
+    title: "kitchen",
+    detail:
+        "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     prize: 19720,
     amount: 50,
-    cover: "https://picsum.photos/600",
+    cover:
+        "https://i.pinimg.com/564x/9d/4d/75/9d4d75304e0bd778cc26d73a7da79a78.jpg",
     images: [
-      "https://picsum.photos/768/1024",
-      "https://picsum.photos/1024/768",
+      "https://i.pinimg.com/564x/ca/eb/bf/caebbf003ce4adf19fbe1829f6ee0f3c.jpg",
+      "https://i.pinimg.com/564x/ce/57/cf/ce57cf8f3854e1dd1fd72c89d0093d43.jpg",
     ],
     colors: [
       Color(0xFFD2B99B),
