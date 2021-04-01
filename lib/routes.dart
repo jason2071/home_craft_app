1b6e24e68b7843e08eca5876a58e0b99166e1539
Phumai <mcc3939@gmail.com>
2021-04-01 10:55:20 +0700

    add route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:home_craft_app/pages/category/category_screen.dart';
+import 'package:home_craft_app/pages/home/home_screen.dart';
 import 'package:home_craft_app/pages/sign_in/sign_in_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  HomeScreen.routeName: (context) => HomeScreen(),
   CategoryScreen.routeName: (context) => CategoryScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
 };
