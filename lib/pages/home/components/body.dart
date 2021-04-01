8d57bf60b3b19950cb94e1f0f043f435b9cfb56a
Phumai <mcc3939@gmail.com>
2021-04-01 10:34:37 +0700

    fix home

---
+++ lib/pages/home/components/body.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:home_craft_app/controllers/category_notifier.dart';
+import 'package:home_craft_app/controllers/trending_notifier.dart';
+import 'package:home_craft_app/models/product_model.dart';
+import 'package:home_craft_app/pages/home/components/category_item.dart';
+import 'package:home_craft_app/pages/home/components/collection_item.dart';
+import 'package:home_craft_app/pages/home/components/trend_item.dart';
+import 'package:home_craft_app/size_config.dart';
+import 'package:provider/provider.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Discover",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(26),
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SizedBox(width: 20),
+                  ...List.generate(
+                    context.watch<CategoryNotifier>().categories.length,
+                    (index) => CategoryItem(
+                      imageSrc: context
+                          .watch<CategoryNotifier>()
+                          .categories[index]
+                          .imageSrc,
+                      title: context
+                          .watch<CategoryNotifier>()
+                          .categories[index]
+                          .title,
+                      amount: context
+                          .watch<CategoryNotifier>()
+                          .categories[index]
+                          .amount,
+                      color: context
+                          .watch<CategoryNotifier>()
+                          .categories[index]
+                          .color,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Text(
+                    "Trending",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "Show all",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.arrow_right),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            ...List.generate(
+              context.watch<TrendingNotifier>().trending.length,
+              (index) {
+                ProductModel item =
+                    context.watch<TrendingNotifier>().trending[index];
+                return TrendItem(
+                  imageSrc: item.cover,
+                  title: item.title,
+                  prize: item.prize.toString(),
+                  onPressed: () {},
+                );
+              },
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 170,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 20),
+                    ...List.generate(3, (index) => CollectionItem()),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 100,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:home_craft_app/controllers/category_notifier.dart';
-import 'package:home_craft_app/controllers/trending_notifier.dart';
-import 'package:home_craft_app/models/product_model.dart';
+import 'package:home_craft_app/pages/home/components/body.dart';
 import 'package:home_craft_app/size_config.dart';
-import 'package:provider/provider.dart';
-
-import 'components/category_item.dart';
-import 'components/collection_item.dart';
-import 'components/trend_item.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -17,7 +10,6 @@ class HomeScreen extends StatelessWidget {
     SizeConfig().init(context);
 
     return Scaffold(
-      // backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -35,115 +27,7 @@ class HomeScreen extends StatelessWidget {
         ],
         backgroundColor: Colors.transparent,
       ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Discover",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(26),
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    SizedBox(width: 20),
-                    ...List.generate(
-                      context.watch<CategoryNotifier>().categories.length,
-                      (index) => CategoryItem(
-                        imageSrc: context
-                            .watch<CategoryNotifier>()
-                            .categories[index]
-                            .imageSrc,
-                        title: context
-                            .watch<CategoryNotifier>()
-                            .categories[index]
-                            .title,
-                        amount: context
-                            .watch<CategoryNotifier>()
-                            .categories[index]
-                            .amount,
-                        color: context
-                            .watch<CategoryNotifier>()
-                            .categories[index]
-                            .color,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 30),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  children: [
-                    Text(
-                      "Trending",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Spacer(),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Text(
-                        "Show all",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Icon(Icons.arrow_right),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              ...List.generate(
-                context.watch<TrendingNotifier>().trending.length,
-                (index) {
-                  ProductModel item =
-                      context.watch<TrendingNotifier>().trending[index];
-                  return TrendItem(
-                    imageSrc: item.cover,
-                    title: item.title,
-                    prize: item.prize.toString(),
-                    onPressed: () {},
-                  );
-                },
-              ),
-              SizedBox(height: 30),
-              Container(
-                height: 170,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 20),
-                      ...List.generate(3, (index) => CollectionItem()),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 100,
-                color: Colors.red,
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Body(),
     );
   }
 }
