72239206e9fb3e6a8b25fd237484670bb2a09868
Phumai <mcc3939@gmail.com>
2021-04-02 17:18:39 +0700

    fix

--- lib/pages/home/components/body.dart
+++ lib/pages/home/components/body.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:home_craft_app/components/slide_text_model.dart';
 import 'package:home_craft_app/components/title_show_all.dart';
 import 'package:home_craft_app/controllers/horizontal_item.dart';
 import 'package:home_craft_app/controllers/new_arrival_notifier.dart';
 import 'package:home_craft_app/pages/show_all/show_all_screen.dart';
 import 'package:provider/provider.dart';
-import 'header_slide.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -17,11 +15,6 @@ class Body extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              HeaderSlide(
-                image:
-                    "https://i.pinimg.com/564x/c9/ba/c6/c9bac67e3204ada8c06f5a6f8b8fa05d.jpg",
-                data: slideData,
-              ),
               TitleShowAll(
                 title: "New Arrivals",
                 onPressed: () {
--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -15,28 +15,43 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       key: _scaffoldKey,
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        brightness: Brightness.dark,
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.white),
-          onPressed: () {
-            _scaffoldKey.currentState.openDrawer();
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.white),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.shopping_bag_outlined, color: Colors.white),
-            onPressed: () {},
-          ),
-        ],
-      ),
       drawer: CustomDrawer(),
-      body: Body(),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 300,
+              floating: false,
+              pinned: false,
+              brightness: Brightness.dark,
+              backgroundColor: Colors.black,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  "https://i.pinimg.com/564x/c9/ba/c6/c9bac67e3204ada8c06f5a6f8b8fa05d.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              leading: IconButton(
+                icon: Icon(Icons.menu, color: Colors.white),
+                onPressed: () {
+                  _scaffoldKey.currentState.openDrawer();
+                },
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.search, color: Colors.white),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.shopping_bag_outlined, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ],
+            )
+          ];
+        },
+        body: Body(),
+      ),
     );
   }
 }
